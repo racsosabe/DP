0d57e60ca7315ec95503f3dd8a03089ca32b5892
racsosabe <racso_galvan@hotmail.com>
2018-01-26 15:17:52 -0500

    Add files via upload

--- DPwithBitmask2.cpp
+++ DPwithBitmask2.cpp
@@ -8,31 +8,31 @@ double v[N][N];
 double memo[N][1<<N];
 bool vis[N][1<<N];
 
-double DP(int last, int bitmask){
-	if(bitmask+1 == int(1<<n)) return 1.0; // Caso final
-	if(vis[last][bitmask]) return memo[last][bitmask]; // Respuesta ya calculada
+double DP(int last, int bm){
+	if(bm+1 == int(1<<n)) return 1.0; // Caso final
+	if(vis[last][bm]) return memo[last][bm]; // Respuesta ya calculada
 	double ans = 0.0; // Minimo posible
 	if(last == 0){ // Caso 1
 		for(int i=0; i<n; i++){
-			if((bitmask>>i)&1) continue; // Ignorar los que ya se tomaron
-			ans = max(ans,1.0*v[last][i]*DP(last,bitmask|int(1<<i)));
+			if((bm>>i)&1) continue; // Ignorar los que ya se tomaron
+			ans = max(ans,1.0*v[last][i]*DP(last,bm|int(1<<i)));
 		}
 	}
 	else{ // Caso 2
 		for(int i=0; i<n; i++){
-			if((bitmask>>i)&1) continue; // Ignorar los que ya se tomaron
+			if((bm>>i)&1) continue; // Ignorar los que ya se tomaron
 			if(i == 0){
-				ans = max(ans,1.0*v[i][last]*DP(i,bitmask|int(1<<i))); // Subcaso 1
+				ans = max(ans,1.0*v[i][last]*DP(i,bm|int(1<<i))); // Subcaso 1
 			}
 			else{
-				double carry = 1.0*v[last][i]*DP(last,bitmask|int(1<<i));
-				carry += 1.0*v[i][last]*DP(i,bitmask|int(1<<i));
+				double carry = 1.0*v[last][i]*DP(last,bm|int(1<<i));
+				carry += 1.0*v[i][last]*DP(i,bm|int(1<<i));
 				ans = max(ans,carry);
 			}
 		}
 	}
-	vis[last][bitmask] = true; // Estado visitado
-	return memo[last][bitmask] = ans; // Almacenar y devolver respuesta
+	vis[last][bm] = true; // Estado visitado
+	return memo[last][bm] = ans; // Almacenar y devolver respuesta
 }
 
 int main(){
