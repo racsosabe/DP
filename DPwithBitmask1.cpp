ce766d5674c0ee6ecbe4f490c3937a87e8f4c6f2
racsosabe <racso_galvan@hotmail.com>
2018-01-26 15:29:25 -0500

    Update DPwithBitmask1.cpp

--- DPwithBitmask1.cpp
+++ DPwithBitmask1.cpp
@@ -9,14 +9,14 @@ double memo[1<<(2*N)], d[2*N][2*N];
 bool vis[1<<(2*N)];
 
 double dp(int bitmask){
-	if(bitmask+1==(int)(1<<(2*n))) return 0.0; // Ya es el ultimo grupo que se forma
+	if(bitmask+1==(int)(1<<(2*n))) return 0.0; // Caso de salida
 	if(vis[bitmask]) return memo[bitmask]; // Respuesta ya calculada
 	int m1=0; // Primer elemento de la pareja a formar
 	while(bitmask&(int)(1<<m1)) m1++;
 	double ans = DBL_MAX;
 	for(int m2 = m1+1; m2 < 2*n; m2++){ // Iterar para obtener el segundo elemento
-		if(int(bitmask&(int)(1<<m2))==0)
-			ans = min(ans,d[m1][m2] + dp(bitmask | (1<<m1) | (1<<m2))); // Minimizar el valor
+		if(int(bitmask&(int)(1<<m2))==0) // Minimizamos
+			ans = min(ans,d[m1][m2] + dp(bitmask|(1<<m1)|(1<<m2)));
 	}
 	vis[bitmask] = true; // Estado visitado
 	return memo[bitmask] = ans; // Almacenar y devolver respuesta
@@ -27,10 +27,10 @@ int main(){
 	int t = 1;
 	while(cin >> n && n!=0){ // Leer los datos hasta el End of File
 		memset(vis,0,sizeof vis);
-		for(int i=0; i<2*n; i++) cin >> name >> x[i] >> y[i]; // Recibir los datos
+		for(int i=0; i<2*n; i++) cin >> name >> x[i] >> y[i];
 		for(int i=0; i<2*n; i++){
-			for(int j=0; j<2*n; j++){
-				d[i][j] = hypot((x[i]-x[j]),(y[i]-y[j])); // Calcular las distancias
+			for(int j=0; j<2*n; j++){ // Calcular distancias
+				d[i][j] = hypot((x[i]-x[j]),(y[i]-y[j])); 
 			}
 		}
 		printf("Case %d: %.2f\n",t++,dp(0));
